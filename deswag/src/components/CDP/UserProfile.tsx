e10a191a8eb3b73aa1c5044796ed6c2a0a6595ad
AzureAD\MichaelZoubkoff <michael.zoubkoff@outlook.com>
2025-08-16 13:20:43 -0400

    edit connect wallet

--- deswag/src/components/CDP/UserProfile.tsx
+++ deswag/src/components/CDP/UserProfile.tsx
@@ -9,9 +9,9 @@ export default function UserProfile() {
   }
 
   return (
-    <div>
+    <div className="absolute end-0 mt-[150px] bg-gray-100 m-4 p-3 rounded-lg">
       <h2>Profile</h2>
-      <p>User ID: {currentUser .userId}</p>
+      <p>User ID: {currentUser.userId}</p>
       <p>Primary Address: {primaryAddress}</p>
       <p>All Accounts: {currentUser?.evmAccounts?.join(", ")}</p>
     </div>
--- deswag/src/components/ConnectWalletButton.tsx
+++ deswag/src/components/ConnectWalletButton.tsx
@@ -1,8 +1,30 @@
-"use client";
+import { useCurrentUser } from "@coinbase/cdp-hooks";
 import { AuthButton } from "@coinbase/cdp-react/components/AuthButton";
+import UserProfile from "./CDP/UserProfile";
+import { useState } from "react";
 
-export default function ConnectWalletButton() {
+export default function WalletStatus() {
+  const { currentUser } = useCurrentUser();
+
+  const [opened, setOpened] = useState(false);
+
+  if (!currentUser) {
+    return <AuthButton className="px-4 py-2 rounded-lg bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50" />;
+  }
+
+  const addr = currentUser.evmAccounts?.[0] ?? "";
+
+  // short form (0x1234...abcd)
+  const short = addr ? `${addr.slice(0, 6)}…${addr.slice(-4)}` : "";
+
   return (
-      <AuthButton />
+    <>
+      {
+        opened ? <UserProfile></UserProfile> : <></>
+      }
+      <div className="ellipsis max-w-36" onClick={() => setOpened((e) => !e)}>
+        {short}
+      </div>
+    </>
   );
 }
